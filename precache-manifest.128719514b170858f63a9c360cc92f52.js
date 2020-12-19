c8ffd0ca1b13339fe36677c8792ffb5068bd1075
danielm2402 <damunoz216@unicauca.edu.co>
2020-12-19 00:36:13 -0500

    Updates

---
+++ precache-manifest.128719514b170858f63a9c360cc92f52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c8d8803171a169d9e800d691e09c2d",
+    "url": "/waves-calculator/index.html"
+  },
+  {
+    "revision": "8d8ea6bc70b771446d1e",
+    "url": "/waves-calculator/static/css/main.c3bcaae7.chunk.css"
+  },
+  {
+    "revision": "2c5a8920490a07db308d",
+    "url": "/waves-calculator/static/js/2.12d1c425.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/waves-calculator/static/js/2.12d1c425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d8ea6bc70b771446d1e",
+    "url": "/waves-calculator/static/js/main.9c2f1132.chunk.js"
+  },
+  {
+    "revision": "85f920b6b350174547cd",
+    "url": "/waves-calculator/static/js/runtime-main.d2e564d3.js"
+  },
+  {
+    "revision": "e46a7579c0d4713e95b3e785cd520d18",
+    "url": "/waves-calculator/static/media/bg1.e46a7579.jpg"
+  },
+  {
+    "revision": "46c908607ed4aa89ef4da2ebea7735d9",
+    "url": "/waves-calculator/static/media/gif-pendulum.46c90860.gif"
+  }
+]);
